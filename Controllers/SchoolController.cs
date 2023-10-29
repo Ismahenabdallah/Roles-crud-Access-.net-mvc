ddc96af4a8ca4e420180bb32f18e709da8817ec4
Ismahenabdallah <abdallah.ismahen99@gmail.com>
2023-10-29 19:54:00 +0100

    Ajoutez des fichiers projet.

---
+++ Controllers/SchoolController.cs
@@ -0,0 +1,109 @@
+﻿using atelier3.Models;
+using atelier3.Models.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace atelier3.Controllers
+{
+    [Authorize(Roles = "Admin,Manager")]
+
+    public class SchoolController : Controller
+         
+    {
+        private ISchoolRepository schoolRepository;
+        public SchoolController(ISchoolRepository schoolRepository)
+        {
+            this.schoolRepository = schoolRepository;
+        }
+        [AllowAnonymous]
+        // GET: SchoolController
+        public ActionResult Index()
+        {
+            var model = schoolRepository.GetAll();
+            return View(model);
+        }
+
+        // GET: SchoolController/Details/5
+        public ActionResult Details(int id)
+        {
+            var model = schoolRepository.GetById(id);
+            return View(model);
+        }
+
+        // GET: SchoolController/Create
+        public ActionResult Create()
+        {
+           
+            return View();
+        }
+
+        // POST: SchoolController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(School s)
+        {
+            try
+            {
+                schoolRepository.Add(s);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: SchoolController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var sh = schoolRepository.GetById(id);
+            if (sh == null)
+            {
+                return NotFound(); 
+            }
+            return View(sh);
+        }
+
+        // POST: SchoolController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, School s)
+        {
+            try
+            {
+                schoolRepository.Edit(s);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+
+
+        }
+
+        // GET: SchoolController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var s = schoolRepository.GetById(id);
+            return View(s);
+        }
+
+        // POST: SchoolController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, School s)
+        {
+            try
+            {
+                schoolRepository.Delete(s);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Controllers/StudentController.cs
@@ -0,0 +1,126 @@
+﻿using atelier3.Models;
+using atelier3.Models.Repositories;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace atelier3.Controllers
+{
+    [Authorize(Roles = "Admin,Manager")]
+    public class StudentController : Controller
+    {
+        private IStudentRepository StudentRepository;
+        private ISchoolRepository schoolRepository;
+        public StudentController(IStudentRepository StudentRepository  , ISchoolRepository schoolRepository)
+        {
+            this.StudentRepository = StudentRepository;
+            this.schoolRepository = schoolRepository;
+        }
+        [AllowAnonymous]
+
+        // GET: StudentController
+        public ActionResult Index()
+        {
+            ViewBag.SchoolID = new SelectList(schoolRepository.GetAll(), "SchoolID", "SchoolName");
+            var model = StudentRepository.GetAll();
+            return View(model);
+        }
+
+        // GET: StudentController/Details/5
+        public ActionResult Details(int id)
+        {
+            var model = StudentRepository.GetById(id);
+            return View(model);
+        }
+
+        // GET: StudentController/Create
+        public ActionResult Create()
+        {
+            ViewBag.SchoolID = new SelectList(schoolRepository.GetAll(),"SchoolID","SchoolName"); 
+            return View();
+        }
+
+        // POST: StudentController/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Student collection)
+        {
+            try
+            {
+                ViewBag.SchoolID = new SelectList(schoolRepository.GetAll(), "SchoolID", "SchoolName" , collection.SchoolID);
+                StudentRepository.Add(collection);
+
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StudentController/Edit/5
+        public ActionResult Edit(int id)
+        {
+            ViewBag.SchoolID = new SelectList(schoolRepository.GetAll(), "SchoolID", "SchoolName");
+            var sh = StudentRepository.GetById(id);
+            if (sh == null)
+            {
+                return NotFound();
+            }
+            return View(sh);
+        }
+
+        // POST: StudentController/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, Student collection)
+        {
+            try
+            {
+                ViewBag.SchoolID = new SelectList(schoolRepository.GetAll(), "SchoolID", "SchoolName", collection.SchoolID);
+                StudentRepository.Edit(collection);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: StudentController/Delete/5
+        public ActionResult Delete(int id)
+        {
+            var s = StudentRepository.GetById(id);
+            return View(s);
+        }
+
+        // POST: StudentController/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, Student collection)
+        {
+            try
+            {
+                StudentRepository.Delete(collection);
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        public ActionResult Search(string name, int? schoolid)
+        {
+            var result = StudentRepository.GetAll();
+            if (!string.IsNullOrEmpty(name))
+                result = StudentRepository.FindByName(name);
+            else
+            if (schoolid != null)
+                result = StudentRepository.GetStudentsBySchoolID(schoolid);
+            ViewBag.SchoolID = new SelectList(schoolRepository.GetAll(), "SchoolID", "SchoolName");
+            return View("Index", result);
+        }
+
+    }
+}
